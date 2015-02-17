f21db8198f51c66585f05cca88733ca06bf7a9ae
Dalal Alnasrallah <dalalalnasrallah@hotmail.com>
2015-02-17 15:34:44 -0800

    up

---
+++ workP/Monopoly/src/edu/ncsu/monopoly/IOwnable.java
@@ -0,0 +1,14 @@
+package edu.ncsu.monopoly;
+
+public interface IOwnable {
+
+	public abstract Player getTheOwner();
+
+	public abstract void setTheOwner(Player owner);
+
+	public abstract void setAvailable(boolean available);
+
+	public abstract boolean isAvailable();
+	
+
+}
